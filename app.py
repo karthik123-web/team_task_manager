4337c306fd85a981eeba867d19fc78dfb6da19ed
Karthik muddam <karthikmuddam97@gmail.com>
2026-05-03 23:15:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -216,5 +216,5 @@ import os
 
 if __name__ == "__main__":
     init_db()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
